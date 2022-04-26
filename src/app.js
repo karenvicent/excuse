3fc6461ac70bbc224e7a18a160f8f5fa2ff0814f
karenvicent <karenvergara@msn.com>
2022-04-26 02:52:05 +0000

    tarea terminada

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generarexcusa();
+};
+let generarexcusa = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let num1 = Math.floor(Math.random() * who.length);
+  let num2 = Math.floor(Math.random() * what.length);
+  let num3 = Math.floor(Math.random() * when.length);
+  return who[num1] + " " + what[num2] + " " + when[num3];
 };
